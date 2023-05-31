4b057951a543f092701ce65576043c52e0e0afa3
ahmadsaufi211001011 <ahmadsaufi2110010119@gmail.com>
2023-05-31 14:02:17 +0630

    selesai pertemuan 3

---
+++ footer.php
@@ -0,0 +1,6 @@
+  </div>
+  <footer>
+      <div class="container">
+          <span>&copy; <?php echo date('Y'); ?> - FTI UNISKA</span>
+      </div>
+  </footer>
---
+++ profile.php
@@ -0,0 +1,52 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/Ahmad Saufi pas foto.jpg" alt="Ahmad Saufi" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">2110010119</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas</strong></td>
+                        <td align="left" width="50%">Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Ahmad Saufi</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin,14 agustus 20010</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0819 5366 6874</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">ahmad5saufi5@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://facebook.com" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com/dedyrosyadi" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="http://twitter.com/dedyrosyadi" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/twitter.svg" width="30" alt="twitter">
+                            </a>
+                            <a href="http://linkedin.com/in/dedyrosyadi" target="_blank">
+                                <img src="img/medsos/linkedin.svg" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
