ec30913a70b3d312c70dc4b51db8d6c27bd608c5
ahmadsaufi211001011 <ahmadsaufi2110010119@gmail.com>
2023-05-31 15:10:57 +0630

    selesai pertemuan 4

--- admin/nav.php
+++ admin/nav.php
@@ -2,6 +2,8 @@
     <li class="list-group-item"><a href="?page=dashboard">Dashboard</a></li>
     <li class="list-group-item"><a href="?page=mahasiswa-show">Data Mahasiswa</a></li>
     <li class="list-group-item"><a href="?page=mahasiswa-add">Tambah Data Mahasiswa</a></li>
+    <li class="list-group-item"><a href="?page=matakuliah-show">Data Mata kuliah</a></li>
+    <li class="list-group-item"><a href="?page=matakuliah-add">Tambah Mata kuliah</a></li>
     <?php
 
     if ($_SESSION['username'] == 'admin') {
---
+++ matakuliah/matakuliah_add.php
@@ -0,0 +1,43 @@
+<?php
+if (isset($_POST['Submit'])) {
+    $kode = $_POST['kode'];
+    $nama = $_POST['nama'];
+    $sks = $_POST['sks'];
+    $semester = $_POST['semester'];
+    $result = mysqli_query($con, "INSERT INTO matakuliah(kode,nama,sks,semester) 
+VALUES('$kode','$nama','$sks','$semester')");
+    header("Location:?page=matakuliah-show") or die(mysqli_error($con));
+}
+?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <strong>Tambah Data Mata kuliah</strong>
+            </div>
+            <div class="card-body">
+                <form method="POST" action="?page=matakuliah-add" class="form-horizontal">
+                    <div class="form-group">
+                        <label for="kode" class="control-label">kode</label>
+                        <input type="text" class="form-control" name="kode" placeholder="Masukan kode..." required>
+                    </div>
+                    <div class="form-group">
+                        <label for="nama" class="ontrol-label">Nama Lengkap</label>
+                        <input type="text" class="form-control" name="nama" placeholder="Masukan Nama Lengkap..." required>
+                    </div>
+                    <div class="form-group">
+                        <label for="sks" class="control-label">sks</label>
+                        <input type="text" class="form-control" name="sks" placeholder="Masukan sks..." required>
+                    </div>
+                    <div class="form-group">
+                        <label for="semester" class="control-label">semester</label>
+                        <input type="semester" class="form-control" name="semester" placeholder="Masukan semester..." required>
+                    </div>
+
+                    <input type="submit" name="Submit" class="btn btn-primary" value="Simpan">
+                    <input type="reset" name="reset" class="btn btn-warning" value="Reset">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
